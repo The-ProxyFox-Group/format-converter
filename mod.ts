e8d4c777c7d3db5e683b9012faca302ccf0d85a0
Oliver <59418903+Oliver-makes-code@users.noreply.github.com>
2022-10-02 17:07:26 -0500

    Validation checks

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
-const systems = JSON.parse(Deno.readTextFileSync("./systems.json"))
-const roles = JSON.parse(Deno.readTextFileSync("./roles.json"))
+const systems = JSON.parse(Deno.readTextFileSync("./systems.json").replaceAll("\\u0000", ""))
+const roles = JSON.parse(Deno.readTextFileSync("./roles.json").replaceAll("\\u0000", ""))
 
 const output: {
     schema: number,
@@ -23,40 +23,55 @@ const output: {
     "servers": {}
 }
 
+const validIdRegex = /^[a-z]{5}$/
+
 for (const j in systems) {
     const system = systems[j]
-    const id = system.id;
+    let id = system.id;
+    if (!validIdRegex.test(id)) {
+        id = generateRandomId()
+    }
+    while (output.systems[id] != undefined) {
+        id = generateRandomId()
+    }
     const obj = {
         id,
         accounts: [j],
-        name: system.name,
-        description: system.description,
-        tag: system.tag,
-        avatarUrl: system.avatar_url,
-        timestamp: system.created,
+        name: system.name == "" ? undefined : system.name,
+        description: system.description == "" ? undefined : system.description,
+        tag: system.tag == "" ? undefined : system.tag,
+        avatarUrl: system.avatar_url == "" ? undefined : system.avatar_url,
+        timestamp: system.created == "" ? undefined : system.created,
         autoType: system.auto_bool? "latch": undefined,
         members: {},
         serverProxy: system.server_proxy,
         proxyTags: [],
-        switches: system.switches
+        switches: []
     }
 
     for (const l in system.members) {
         const member = system.members[l]
-        const memid = member.id
+        let memid = member.id
+        if (!validIdRegex.test(memid)) {
+            memid = generateRandomId()
+        }
+        //@ts-ignore aaaaaaaaaaaaaaaaaaaaaaaaaa
+        while (obj.members[memid] != undefined) {
+            memid = generateRandomId()
+        }
         //@ts-ignore technically doesn't exist in type
         obj.members[memid] = {
             id: memid,
-            name: member.name,
-            displayName: member.dispaly_name,
-            description: member.description,
-            birthday: member.birthday,
-            pronouns: member.pronouns,
-            color: member.color,
-            avatarUrl: member.avatar_url,
+            name: typeof member.name != "string" || member.name == "" ? memid : member.name,
+            displayName: member.dispaly_name == "" ? undefined : member.displayName,
+            description: member.description == "" ? undefined : member.description,
+            birthday: member.birthday == "" ? undefined : member.birthday,
+            pronouns: member.pronouns == "" ? undefined : member.pronouns,
+            color: member.color == "" ? undefined : member.color,
+            avatarUrl: member.avatar_url == "" ? undefined : member.avatar_url,
             keepProxy: member.keep_proxy,
             messageCount: member.message_count,
-            timestamp: member.created,
+            timestamp: member.created == "" ? undefined : member.timestamp,
             serverDisplayName: member.server_nick,
             serverAvatarUrl: member.server_avatar,
             serverProxy: member.server_proxy
@@ -80,3 +95,15 @@ for (const p in roles) {
 }
 
 Deno.writeTextFileSync("./systems_output.json", JSON.stringify(output))
+
+function generateRandomId(): string {
+    let out = ""
+    for (let i = 0; i < 5; i++) {
+        out += String.fromCharCode(random(0,26)+97)
+    }
+    return out
+}
+
+function random(min: number, max: number) {
+    return Math.random() * (max - min) + min
+}
